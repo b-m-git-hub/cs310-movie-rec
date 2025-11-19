b6a7f0d1cec2449f32a32c5ab257c417afd7bb46
Bryan McMahon <bryan.mcmahon4698@gmail.com>
2025-11-19 18:36:55 -0500

    Added similarity column

--- data_processing.py
+++ data_processing.py
@@ -1,16 +1,19 @@
 import pandas as pd
+import numpy as np
 
 file_path = 'processed_movie_data.csv'
 
 def load_data(file_path):
     #columns = ['id', 'title', 'genres', 'keywords', 'popularity']
     movieData = pd.read_csv(file_path) #usecols=columns
+    movieData['similarity'] = np.nan
     return movieData
 
 def main():
     movieData = load_data(file_path)
     pd.set_option('display.max_columns', None)
     #pd.reset_option('display.max_columns', None)
+    #movieData.to_csv('processed_movie_data.csv', index=False)
     print(movieData.head())
 
 if __name__ == "__main__":
