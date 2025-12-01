aea7f859e72fe2b0929cc75334841f9e0db0dfeb
Bryan McMahon <bryan.mcmahon4698@gmail.com>
2025-12-01 15:55:30 -0500

    Updated data processing to clean empty columns, updated CSV file with cleaned up columns

--- data_processing.py
+++ data_processing.py
@@ -3,15 +3,18 @@ import pandas as pd
 file_path = 'processed_movie_data.csv'
 
 def load_data(file_path):
-    #columns = ['id', 'title', 'genres', 'keywords', 'popularity', 'similarity']
-    movieData = pd.read_csv(file_path) #usecols=columns
+    # Read CSV file
+    movieData = pd.read_csv(file_path)
+    # Clean up if both the genre and keyword columns are empty
+    movieData.dropna(subset=['genres', 'keywords'], how='all', inplace=True)
     return movieData
 
 def main():
     movieData = load_data(file_path)
+    # Shows all columns when printing
     pd.set_option('display.max_columns', None)
-    #pd.reset_option('display.max_columns', None)
-    #movieData.to_csv('processed_movie_data.csv', index=False)
+    # Saves the data, will overwrite the existing file
+    movieData.to_csv('processed_movie_data.csv', index=False)
     print(movieData.head())
 
 if __name__ == "__main__":
